20adb79da519a9506ce52d56b83926aef3e5c2f5
Juan Silva <juan.silva.0270@gmail.com>
2025-07-28 11:00:27 -0300

    Agregar pruebas para el caso de uso de registro de usuario con manejo de email ya en uso

---
+++ domain/src/use-cases/user-register.test.ts
@@ -0,0 +1,22 @@
+import { describe, test, expect } from 'vitest';
+import { userRegister } from './user-register';
+
+describe('UserRegister Use Case', () => {
+    test('whit an email already in use, fails with InvalidData', () => {
+        const payload = {
+            email: 'test@test.com',
+            name: 'Test User',
+            telephone: '123456789',
+            role: 'client',
+            password: 'password123',
+        } 
+
+        const result = userRegister(payload);
+        expect(result).toEqual({
+            error: {
+                code: 'InvalidData',
+                message: 'Email already in use',
+            }
+        })
+    });
+})
