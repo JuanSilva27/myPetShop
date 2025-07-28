298e508fd8adc3f40efaf5e84cb90d79cd647bbc
Juan Silva <juan.silva.0270@gmail.com>
2025-07-28 11:26:48 -0300

    Agregar validación para el campo de email en la función userRegister

--- domain/src/use-cases/user-register.ts
+++ domain/src/use-cases/user-register.ts
@@ -1,4 +1,16 @@
-export function userRegister() {
+import { User } from "../entities/User"
+
+export type UserRegisterRequestModel = Omit<User, 'id' | 'role'>
+
+export function userRegister({ email}: UserRegisterRequestModel) {
+    if(!email && email.trim() === '' ) {
+        return {
+            error: {
+                code: 'InvalidData',
+                message: 'Email is required',
+            }
+        }
+    }
   return {
     error: {
       code: 'InvalidData',
