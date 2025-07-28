1eb74b8cb47feafdf9774e613ce4f4b9dcf7f7bf
Juan Silva <juan.silva.0270@gmail.com>
2025-07-28 11:39:32 -0300

    Agregar validación para el campo de contraseña en la función userRegister

--- domain/src/use-cases/user-register.ts
+++ domain/src/use-cases/user-register.ts
@@ -2,15 +2,23 @@ import { User } from "../entities/User"
 
 export type UserRegisterRequestModel = Omit<User, 'id' | 'role'>
 
-export function userRegister({ email}: UserRegisterRequestModel) {
-    if(!email && email.trim() === '' ) {
-        return {
-            error: {
-                code: 'InvalidData',
-                message: 'Email is required',
-            }
-        }
+export function userRegister({ email, password }: UserRegisterRequestModel) {
+  if (!email && email.trim() === '') {
+    return {
+      error: {
+        code: 'InvalidData',
+        message: 'Email is required',
+      }
     }
+  }
+  if (!password && password.trim() === '') {
+    return {
+      error: {
+        code: 'InvalidData',
+        message: 'Password is required',
+      }
+    }
+  }
   return {
     error: {
       code: 'InvalidData',
